318d89cf7cf19a4d45dca5a91d105a78824a4d92
nikkuz39 <84978893+nikkuz39@users.noreply.github.com>
2022-02-22 17:26:51 +0200

    Add project files.

---
+++ WebDriverConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+
+namespace WebDriverConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string department = "Разработка продуктов";
+
+            // Английский - 1
+            // Русский - 2
+            // Немецкий - 3
+            int language = 1;
+
+            WebDriverCore webDriverCore = new WebDriverCore(department, language);
+            webDriverCore.TestProgram();            
+        }
+    }
+}
---
+++ WebDriverConsoleApp/WebDriverCore.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace WebDriverConsoleApp
+{
+    class WebDriverCore
+    {
+        private string department;
+        private int language;
+
+        private ChromeDriver ChromeSession()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            var driver = new ChromeDriver();
+
+            return driver;
+        }
+
+        public WebDriverCore(string dep, int lang)
+        {
+            department = dep;
+            language = lang;
+        }
+
+        public void TestProgram()
+        {
+            var driver = ChromeSession();
+
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://careers.veeam.ru/vacancies");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+
+            // Закрытие всплывающего окна
+            driver.FindElement(By.XPath("//*[@id='cookiescript_close']")).Click();
+
+            Thread.Sleep(1000);
+            // Открытие меню выбора отдела
+            driver.FindElement(By.XPath("//*[@id='sl']")).Click();
+
+            // Выравнивание высоты экрана по меню
+            IJavaScriptExecutor javaScript = (IJavaScriptExecutor)driver;
+            var elementOne = driver.FindElement(By.XPath("//*[@id='root']/div/div[1]/div/div[2]"));
+            javaScript.ExecuteScript("arguments[0].scrollIntoView();", elementOne);
+
+            Thread.Sleep(1000);
+            // Выбор отдела
+            driver.FindElement(By.LinkText(department)).Click();
+
+            Thread.Sleep(1000);
+            // Открытие меню выбора языка
+            driver.FindElement(By.XPath("//*[@id='root']/div/div[1]/div/div[2]/div[1]/div/div[3]/div/div")).Click();
+
+            Thread.Sleep(1000);
+            // Выбор языка
+            driver.FindElement(By.XPath($"//*[@id='root']/div/div[1]/div/div[2]/div[1]/div/div[3]/div/div/div/div[{language}]/label")).Click();
+
+            Thread.Sleep(1000);
+            // Подсчет вакансий
+            int vacanciesCount = driver.FindElement(By.XPath("//*[@id='root']/div/div[1]/div/div[2]/div[2]/div")).FindElements(By.XPath("//*[@id='root']/div/div[1]/div/div[2]/div[2]/div/a")).Count;
+
+            Console.WriteLine($"Vacancies: {vacanciesCount}");
+
+            Console.ReadLine();
+
+            driver.Quit();
+        }
+    }
+}
